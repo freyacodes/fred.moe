0d6f659c3d1f9556db964540138c256f1048dbfe
Frederik Ar. Mikkelsen <frogkr@gmail.com>
2017-05-11 15:39:09 +0200

    Maybe fixed proc builder

--- src/main/java/com/frederikam/fred/moe/Caddy.java
+++ src/main/java/com/frederikam/fred/moe/Caddy.java
@@ -20,7 +20,7 @@ public class Caddy extends Thread {
             log.info("Starting Caddy");
 
             ProcessBuilder pb = new ProcessBuilder()
-                    .command("caddy", "-conf Caddyfile", "-agree", "-email $CADDY_EMAIL");
+                    .command("caddy", "-conf", "Caddyfile", "-agree", "-email", "$CADDY_EMAIL");
             Process proc = pb.start();
             new SLF4JInputStreamLogger(log, proc.getInputStream()).start();
 
